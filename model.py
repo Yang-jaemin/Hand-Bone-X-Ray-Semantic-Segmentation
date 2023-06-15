8c9262200117c976f0d407e242a8af0da4418f14
bogeoung <naturalbb360@gmail.com>
2023-06-15 05:38:06 +0000

    feat: add models(fcn_resnet101_NotPretrained, DeepLabB3_MobileNet_v3_Large)

--- model.py
+++ model.py
@@ -31,6 +31,34 @@ class fcn_resnet101(nn.Module):
         return x
 
 
+class fcn_resnet101_NotPretrained(nn.Module):
+    def __init__(self, classes):
+        super(fcn_resnet101_NotPretrained, self).__init__()
+
+        self.backbone = models.segmentation.fcn_resnet101(pretrained=False)
+        self.backbone.classifier[4] = nn.Conv2d(512, len(classes), kernel_size=1)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
+    
+
+class fcn_resnet101_NotPretrained_1024(nn.Module):
+    def __init__(self, classes):
+        super(fcn_resnet101_NotPretrained_1024, self).__init__()
+
+        self.backbone = models.segmentation.fcn_resnet101(pretrained=False)
+        self.backbone.classifier[0] = nn.Conv2d(2048, 1024, kernel_size=(3,3), stride=(1, 1), padding=1, bias=False)
+        self.backbone.classifier[1] = nn.BatchNorm2d(1024, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        self.backbone.classifier[2] = nn.SELU()
+        self.backbone.classifier[3] = nn.Dropout(p=0.1, inplace=False)
+        self.backbone.classifier[4] = nn.Conv2d(1024, len(classes), kernel_size=(1, 1), stride=(1, 1))
+
+    def forward(self, x):
+        x = self.backbone(x)
+        return x
+
+
 class DeepLabV3_resnet50(nn.Module):
     def __init__(self, num_classes):
         super(DeepLabV3_resnet50, self).__init__()
@@ -51,6 +79,30 @@ class DeepLabV3_resnet101(nn.Module):
         self.backbone.classifier[-1] = nn.Conv2d(256, len(num_classes), kernel_size=1)
 
     def forward(self, x):
+        x = self.backbone(x)
+        return x
+
+
+# pip install --upgrade torch torchvision
+class DeepLabV3_MobileNet_v3_Large(nn.Module):
+    def __init__(self, classes):
+        super(DeepLabV3_MobileNet_v3_Large,self).__init__()
+
+        self.backbone = models.segmentation.deeplabv3.deeplabv3_mobilenet_v3_large(pretrained=True)
+        self.backbone.classifier[-1]=nn.Conv2d(256, len(classes), kernel_size=1)
+    
+    def forward(self,x):
+        x = self.backbone(x)
+        return x
+
+
+class SMP_FPN(nn.Module):
+    def __init__(self, classes):
+        super(SMP_FPN,self).__init__()
+
+        self.backbone = smp.FPN(classes = len(classes))
+
+    def forward(self,x):
         x = self.backbone(x)
         return x
 
